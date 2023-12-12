a113030b8194f897891ebaef47cab5c2bf1b4872
Santino <bournotsantino03@gmail.com>
2023-12-12 16:06:53 -0300

    Cambios en el servicio, 12-12-2023

--- gameSphere/src/app/Services/service.service.ts
+++ gameSphere/src/app/Services/service.service.ts
@@ -1,34 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { tap } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
 
-apiUrl: string = 'http://localhost:8080/log/';
+  apiUrl: string = 'http://localhost:8080/log';
 
   constructor(private http: HttpClient) { }
 
-  persona(id: number, name: string, password: string, points: string){
-    const personaData ={
-      id: id, //Creo objeto id llamando a persona
+  register(name: any) {
+    return this.http.post(`${this.apiUrl}/new`, name).pipe(
+      catchError(error => {
+        console.error('Error during registration:', error);
+        return throwError(error);
+      })
+    );
+  }
+
+  login(id: number, name: string, password: string, points: string) {
+    const userData = {
+      id: id,
       name: name,
       password: password,
       points: points
     };
 
-   /* Funcion para el login (No esta hecho todavía)
-   return this.http.post(`${this.apiUrl}/login`, personaData).pipe(
-      tap(() => {
-        
-         this.sessionService.login(username);  esto no va
+    return this.http.post(`${this.apiUrl}/login`, userData).pipe(
+      catchError(error => {
+        console.error('Error during login:', error);
+        return throwError(error);
       })
     );
-    */
-  }
-  new(name: any) {
-    return this.http.post(`${this.apiUrl}/log`, name); //Trabajo de balma por hoy terminados
   }
 }
