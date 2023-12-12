756454bc8c62033450c065c1f4894c27b59d8735
Santino <bournotsantino03@gmail.com>
2023-12-12 16:05:10 -0300

    Estilos del formulario, 12-12-2023

--- gameSphere/src/app/Components/Usuarios/registro/registro.component.ts
+++ gameSphere/src/app/Components/Usuarios/registro/registro.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ServiceService } from 'src/app/Services/service.service'; 
+import { ServiceService } from 'src/app/Services/service.service';
 
 @Component({
   selector: 'app-registro',
@@ -8,30 +8,67 @@ import { ServiceService } from 'src/app/Services/service.service';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent {
+  @ViewChild('passwordInput') passwordInput!: ElementRef<HTMLInputElement>;
 
   constructor(private http: HttpClient, private serviceService: ServiceService) {}
 
-  new(name: any) {
-    // Accede a la URL base a través del servicio ServiceService
-    return this.http.post(`${this.serviceService.apiUrl}/log/`, name); // Trabajo de balma por hoy terminado
+  username: string = '';
+  password: string = '';
+  isPasswordVisible = false;
+
+  togglePassword() {
+    const input = this.passwordInput.nativeElement;
+    this.isPasswordVisible = !this.isPasswordVisible;
+
+    if (this.isPasswordVisible) {
+      input.type = 'text';
+    } else {
+      input.type = 'password';
+    }
   }
 
-    // Función para realizar una nueva acción (ajusta según tus necesidades)
-    realizarAccion(name: any) {
-      this.http.post(`${this.serviceService.apiUrl}/log/`, name).subscribe(
-        (response) => {
-          console.log('Solicitud exitosa:', response);
-          // Aquí puedes realizar acciones adicionales después de una solicitud exitosa
-        },
-        (error) => {
-          console.error('Error en la solicitud:', error);
-          // Manejo de errores o acciones adicionales en caso de error
-        }
-      );
-    }
-  
-    // Función para realizar la solicitud nueva
-    /*new(name: any) {
-      this.realizarAccion(name);
-    }*/
+  registrarUsuario(event: Event) {
+    event.preventDefault(); 
+
+    const userData = {
+      name: this.username, //Funcion que esta en el boton de ingresar
+      password: this.password
+    };
+
+    // Llamada al método de registro del servicio
+    this.serviceService.register(userData).subscribe(
+      (response) => {
+        console.log('Registro exitoso:', response);
+        // Realizar acciones adicionales después de un registro exitoso si es necesario
+      },
+      (error) => {
+        console.error('Error durante el registro:', error);
+        // Manejo de errores o acciones adicionales en caso de error
+      }
+    );
+  }
 }
+
+
+
+  /*handleLogin(event: Event) {
+    event.preventDefault();
+
+    const userData = {
+      username: this.username,
+      password: this.password
+    };
+    // Resto de tu lógica
+
+    this.http.post(`${this.serviceService.apiUrl}/log/`, { username: this.username, password: this.password })
+    .subscribe(
+      (response) => {
+        console.log('Solicitud exitosa:', response);
+        // Aca se puede realizar acciones adicionales después de una solicitud exitosa
+      },
+      (error) => {
+        console.error('Error en la solicitud:', error);
+        // Manejo de errores o acciones adicionales en caso de error
+      }
+    );
+  }*/
